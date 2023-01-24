89212083c686b73620a95e3de5f819abf9fc26fd
Imron <adamazai@gmail.com>
2023-01-23 16:03:46 -0800

    Feature 11 complete

--- accounts/views.py
+++ accounts/views.py
@@ -10,17 +10,19 @@ def create_user(request):
         if signup_form.is_valid():
             username = signup_form.cleaned_data["username"]
             password = signup_form.cleaned_data["password"]
-            password_confirmation = signup_form.cleaned_data["password_confirmation"]
+            password_confirmation = signup_form.cleaned_data[
+                "password_confirmation"
+            ]
             if password != password_confirmation:
                 return "the passwords do not match"
             User.objects.create_user(username, password=password)
             return redirect("list_projects")
     else:
         signup_form = SignUpForm()
-    context = {
-        "signup_form": signup_form
-    }
+    context = {"signup_form": signup_form}
     return render(request, "accounts/signup.html", context)
+
+
 # Create a user account based off username and passwords being less than 150
 # characters if the password meets requirements then new user is created
 # and redirected to home page
@@ -41,10 +43,14 @@ def show_login(request):
         login_form = LogInForm()
     context = {"login_form": login_form}
     return render(request, "accounts/login.html", context)
+
+
 # Will show the form for pre-registered users to login
 
 
 def logout_user(request):
     logout(request)
     return redirect("login")
+
+
 # Logs out the current user
